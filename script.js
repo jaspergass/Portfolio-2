e09060f87ffcd610a0568ad1a4b129870f9be9a7
jasperjg <ejg11@sfu.ca>
2023-05-14 21:35:52 -0700

    Init

---
+++ script.js
@@ -0,0 +1,11 @@
+let cursor = document.getElementById("cursor");
+
+document.addEventListener("mousemove", (e) => {
+  let x = e.clientX;
+  let y = e.clientY;
+  cursor.style.left = x + "px";
+  cursor.style.top = y + "px";
+  
+  // cursor.style.animation = "cursor-scale 2s ease-out"
+});
+
