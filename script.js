6c188ec22ab6e7bdd00673f06a07f43c7b6e459a
jasperjg <ejg11@sfu.ca>
2023-05-18 12:09:13 -0700

    Video Size and Cursor
    video banner is sized properly, cursor is working

--- script.js
+++ script.js
@@ -1,11 +1,53 @@
-let cursor = document.getElementById("cursor");
+// let cursor = document.querySelector(".cursor");
 
-document.addEventListener("mousemove", (e) => {
-  let x = e.clientX;
-  let y = e.clientY;
-  cursor.style.left = x + "px";
-  cursor.style.top = y + "px";
+// document.addEventListener("mousemove", (e) => {
+//   let x = e.clientX;
+//   let y = e.clientY;
+//   cursor.style.left = x + "px";
+//   cursor.style.top = y + "px";
   
-  // cursor.style.animation = "cursor-scale 2s ease-out"
-});
+//   // cursor.style.animation = "cursor-scale 2s ease-out"
+// });
 
+//paste
+document.addEventListener("DOMContentLoaded", function(event) {
+  var cursor = document.querySelector(".cursor");
+  var links = document.querySelectorAll("a");
+  var initCursor = false;
+
+  for (var i = 0; i < links.length; i++) {
+    var selfLink = links[i];
+
+    selfLink.addEventListener("mouseover", function() {
+      cursor.classList.add("custom-cursor--link");
+    });
+    selfLink.addEventListener("mouseout", function() {
+      cursor.classList.remove("custom-cursor--link");
+    });
+  }
+
+  window.onmousemove = function(e) {
+    var mouseX = e.clientX;
+    var mouseY = e.clientY;
+
+    if (!initCursor) {
+      // cursor.style.opacity = 1;
+      TweenLite.to(cursor, 0.3, {
+        opacity: 1
+      });
+      initCursor = true;
+    }
+
+    TweenLite.to(cursor, 0, {
+      top: mouseY + "px",
+      left: mouseX + "px"
+    });
+  };
+
+  window.onmouseout = function(e) {
+    TweenLite.to(cursor, 0.3, {
+      opacity: 0
+    });
+    initCursor = false;
+  };
+});
