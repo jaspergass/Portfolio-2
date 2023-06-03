e4a271059a24ac70e6d05cf26ff1831e6e082bf7
jasperjg <ejg11@sfu.ca>
2023-06-02 19:35:44 -0700

    Photo gallery update

--- script.js
+++ script.js
@@ -66,3 +66,87 @@ function showMenu() {
     button.innerHTML = "Menu";
   }
 }
+
+//paste
+Vue.component('vue-gallery', {
+  props: ['photos'],
+  data: function () {
+    return {
+      activePhoto: null
+    }
+  },
+  template: `
+    <div class="vueGallery">
+    <div class="activePhoto" :style="'background-image: url('+photos[activePhoto]+');'">
+      <button type="button" aria-label="Previous Photo" class="previous" @click="previousPhoto()">
+        <i class="fas fa-chevron-circle-left"></i>
+      </button>
+      <button type="button" aria-label="Next Photo" class="next" @click="nextPhoto()">
+        <i class="fas fa-chevron-circle-right"></i>
+      </button>
+    </div>
+    <div class="thumbnails">
+      <div
+           v-for="(photo, index) in photos"
+           :src="photo"
+           :key="index"
+           @click="activePhoto = index"
+           :class="{'active': activePhoto == index}" :style="'background-image: url('+photo+')'">
+      </div>
+    </div>
+  </div>`,
+  mounted () {
+    this.activePhoto = 0
+    document.addEventListener("keydown", (event) => {
+      if (event.which == 37)
+        this.previousPhoto()
+      if (event.which == 39)
+        this.nextPhoto()
+    })
+  },
+  methods: {
+    nextPhoto () {
+      this.activePhoto = ( this.activePhoto+1 < this.photos.length ? this.activePhoto+1 : 0 )
+    },
+    previousPhoto () {
+      this.activePhoto = ( this.activePhoto-1 >= 0 ? this.activePhoto-1 : this.photos.length-1 )
+    }
+  }
+})
+
+new Vue({
+  el: '#app',
+  data: {
+    fogphotos: [
+      '../img/fog/fog1.jpg',
+      '../img/fog/fog2.jpg',
+      '../img/fog/fog3.jpg',
+      '../img/fog/fog4.jpg',
+      '../img/fog/fog5.jpg'
+    ],
+    photos2: [
+      '../img/insects/gallery1-1x.jpg',
+      '../img/insects/gallery2-1x.jpg',
+      '../img/insects/gallery3-1x.jpg',
+      '../img/insects/gallery4-1x.jpg',
+      '../img/insects/gallery5-1x.jpg',
+      '../img/insects/gallery6-1x.jpg'
+    ]
+  }
+});
+
+
+
+
+function setPhotos(n){
+  var gallery1 = document.getElementById("gallery1");
+  var gallery2 = document.getElementById("gallery2");
+  
+  if(n==1){
+    gallery1.style.display = "block";
+    gallery2.style.display ="none";
+  }else if(n==2){
+    gallery1.style.display = "none";
+    gallery2.style.display ="block";
+  }
+}
