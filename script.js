3641e8c31a12ddf75838640646d26f95017aa5fd
jasperjg <ejg11@sfu.ca>
2023-07-11 13:37:38 -0700

    Feedback Edits
    Mostly text edits.

--- script.js
+++ script.js
@@ -115,3 +115,9 @@ function showHide(ele) {
       return false;
   }
 }
+
+//scroll
+const scroller = new LocomotiveScroll({
+  el: document.querySelector('[data-scroll-container]'),
+  smooth: true
+});
